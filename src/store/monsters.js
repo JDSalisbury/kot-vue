75e745e912c2e157f1c0703a7e1a81669463c3d9
Jeff <salisbury.jeffery@gmail.com>
2020-05-13 16:18:37 -0400

    Adding/Removing monsters to state.

--- src/store/monsters.js
+++ src/store/monsters.js
@@ -1,10 +1,32 @@
 const state = {
-  monsters: ["Barakka", "SubZero"],
+  monsters: [],
 };
 
-const mutations = {};
+const mutations = {
+  addSelectedMonster(state, monster) {
+    state.monsters.push(monster);
+  },
+  removeSelectedMonster(state, id) {
+    state.monsters = state.monsters.filter((monster) => {
+      return monster.id != id;
+    });
+  },
+};
 
-const actions = {};
+const actions = {
+  updateSelectedMonster(context, monster) {
+    if (monster.checked) {
+      // monster checked, so we want to add this monster to our list of 'selectedmonsters'
+      context.commit("addSelectedMonster", {
+        id: monster.id,
+        name: monster.value,
+      });
+    } else {
+      // otherwise, remove the monster from our list
+      context.commit("removeSelectedMonster", monster.id);
+    }
+  },
+};
 
 const getters = { selectedMonsters: (state) => state };
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,65 @@
   <div class="home">
     <h1>Choose your monsters</h1>
     <div>
-      <input type="checkbox" id="kitty" name="kitty" />
+      <input
+        type="checkbox"
+        name="kitty"
+        :id="6"
+        :value="`Cyber Kitty`"
+        @change="updateSelectedMonster"
+      />
       <label for="kitty">Cyber Kitty</label>
     </div>
     <div>
-      <input type="checkbox" id="penguin" name="penguin" />
+      <input
+        type="checkbox"
+        name="penguin"
+        :id="5"
+        :value="`Space Penguin`"
+        @change="updateSelectedMonster"
+      />
       <label for="penguin">Space Penguin</label>
     </div>
     <div>
-      <input type="checkbox" id="zaur" name="zaur" />
+      <input
+        type="checkbox"
+        name="zaur"
+        :id="4"
+        :value="`Giga Zaur`"
+        @change="updateSelectedMonster"
+      />
       <label for="zaur">Giga Zaur</label>
     </div>
     <div>
-      <input type="checkbox" id="dragon" name="dragon" />
+      <input
+        type="checkbox"
+        name="dragon"
+        :id="3"
+        :value="`Meka Dragon`"
+        @change="updateSelectedMonster"
+      />
       <label for="dragon">Meka Dragon</label>
     </div>
     <div>
-      <input type="checkbox" id="king" name="king" />
+      <input
+        type="checkbox"
+        name="king"
+        :id="2"
+        :value="`The King`"
+        @change="updateSelectedMonster"
+      />
       <label for="king">The King</label>
     </div>
     <div>
-      <input type="checkbox" id="alienoid" name="alienoid" />
+      <input
+        type="checkbox"
+        name="alienoid"
+        :id="1"
+        :value="`Alienoid`"
+        @change="updateSelectedMonster"
+      />
       <label for="alienoid">Alienoid</label>
     </div>
-    <button>Battle</button>
     <p>{{ selectedMonsters.monsters }}</p>
   </div>
 </template>
@@ -33,12 +68,19 @@
 <script>
 // @ is an alias to /src
 import { mapGetters } from "vuex";
+
 export default {
   name: "Home",
   components: {},
   created() {},
-  methods: {},
-  computed: mapGetters(["selectedMonsters"]),
+  methods: {
+    updateSelectedMonster(e) {
+      this.$store.dispatch("updateSelectedMonster", e.target);
+    },
+  },
+  computed: {
+    ...mapGetters(["selectedMonsters"]),
+  },
 };
 </script>
 
