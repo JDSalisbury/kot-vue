67dcb674177e3c11703a4ff7aeea28bec3e6365f
Jeff <salisbury.jeffery@gmail.com>
2020-05-19 21:32:12 -0400

    Adding comp to handle list stuff.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,94 +1,21 @@
 <template>
   <div class="home">
-    <div v-if="!monsterState.submitted">
-      <h1>Choose your monsters</h1>
-      <div>
-        <input
-          type="checkbox"
-          name="kitty"
-          :id="6"
-          :value="`Cyber Kitty`"
-          @change="updateSelectedMonster"
-        />
-        <label for="kitty">Cyber Kitty</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="penguin"
-          :id="5"
-          :value="`Space Penguin`"
-          @change="updateSelectedMonster"
-        />
-        <label for="penguin">Space Penguin</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="zaur"
-          :id="4"
-          :value="`Giga Zaur`"
-          @change="updateSelectedMonster"
-        />
-        <label for="zaur">Giga Zaur</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="dragon"
-          :id="3"
-          :value="`Meka Dragon`"
-          @change="updateSelectedMonster"
-        />
-        <label for="dragon">Meka Dragon</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="king"
-          :id="2"
-          :value="`The King`"
-          @change="updateSelectedMonster"
-        />
-        <label for="king">The King</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="alienoid"
-          :id="1"
-          :value="`Alienoid`"
-          @change="updateSelectedMonster"
-        />
-        <label for="alienoid">Alienoid</label>
-      </div>
-      <h2 v-if="monsterState.subError">{{ monsterState.subError }}</h2>
-      <button @click="submitMonsters">BATTLE!</button>
-    </div>
+    <MonsterList />
     <p>{{ monsterState.monsters }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Home",
-  components: {},
-  created() {},
-  methods: {
-    ...mapActions(["updateSelectedMonster", "submit"]),
-
-    updateSelectedMonster(e) {
-      // https://stackoverflow.com/questions/42686388/how-to-bind-checkboxes-to-vuex-store
-      this.$store.dispatch("updateSelectedMonster", e.target);
-    },
-
-    submitMonsters() {
-      this.$store.dispatch("submitMonsters");
-    },
+  components: {
+    MonsterList: () => import("@/components/home/checkboxList.vue"),
   },
+  created() {},
+  methods: {},
   computed: {
     ...mapGetters(["monsterState"]),
   },
